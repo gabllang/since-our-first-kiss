c307e194e8247e0192fe4798c7ec76d99102599e
Gabriel <davgabie@gmail.com>
2023-09-08 13:00:59 -0300

    mudei o metodo e adicionei horas minutos e segundo

--- FirstKiss/src/CalculadoraTempo.java
+++ FirstKiss/src/CalculadoraTempo.java
@@ -1,24 +1,28 @@
-import java.time.LocalDate;
-import java.time.Period;
+import java.time.LocalDateTime;
+import java.time.Duration;
 
 public class CalculadoraTempo {
     public static void main(String[] args) {
-        // Defina a data de referência (18 de janeiro de 2022)
-        LocalDate dataReferencia = LocalDate.of(2023, 1, 18);
+        // Defina a data e hora de referência (18 de janeiro de 2022, 00:00:00)
+        LocalDateTime dataReferencia = LocalDateTime.of(2023, 1, 18, 0, 0, 0);
 
-        // Obtenha a data atual
-        LocalDate dataAtual = LocalDate.now();
+        // Obtenha a data e hora atual
+        LocalDateTime dataAtual = LocalDateTime.now();
 
-        // Calcule a diferença entre as datas
-        Period periodo = Period.between(dataReferencia, dataAtual);
+        // Calcule a diferença entre as datas e horas
+        Duration duracao = Duration.between(dataReferencia, dataAtual);
 
-        // Extraia os anos, meses e dias da diferença
-        int anos = periodo.getYears();
-        int meses = periodo.getMonths();
-        int dias = periodo.getDays();
+        // Extraia os anos, meses, dias, horas, minutos e segundos da diferença
+        long anos = duracao.toDays() / 365;
+        long meses = (duracao.toDays() % 365) / 30;
+        long dias = duracao.toDays() % 30;
+        long horas = duracao.toHours() % 24;
+        long minutos = duracao.toMinutes() % 60;
+        long segundos = duracao.getSeconds() % 60;
 
         // Exiba os resultados
-        System.out.println("Desde 18 de janeiro de 2022 até hoje:");
-        System.out.println(anos + " anos, " + meses + " meses e " + dias + " dias.");
+        System.out.println();
+        System.out.println("Desde o dia do nosso primeiro beijo até agora:");
+        System.out.println(anos + " anos, " + meses + " meses, " + dias + " dias, " + horas + " horas, " + minutos + " minutos e " + segundos + " segundos.");
     }
 }
